4bb2469ce07f1ceb40b83d0a0d5d7b8888879540
marko <marko.schrenker@googlemail.com>
2017-10-19 11:05:55 +0200

    We now say via our own testrunner, that our TestApplication is to use, when testing.

---
+++ app/src/androidTest/java/minsky/question/app/TestAppComponent.java
@@ -0,0 +1,40 @@
+package minsky.question.app;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+import dagger.android.support.AndroidSupportInjectionModule;
+import minsky.question.app.di.ActivityBindingModule;
+import minsky.question.app.di.AppComponent;
+
+@Singleton
+@Component(modules = {
+        TestAppModule.class,
+        TestNetworkApiModule.class,
+        ActivityBindingModule.class,
+        AndroidSupportInjectionModule.class
+})
+public interface TestAppComponent extends AndroidInjector<DaggerApplication> {
+
+    void inject(TestMyApplication app);
+
+    @Override
+    void inject(DaggerApplication instance);
+
+    // Gives us syntactic sugar. we can then do DaggerAppComponent.builder().application(this).build().inject(this);
+    // never having to instantiate any modules or say which module we are passing the application to.
+    // Application will just be provided into our app graph now.
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        TestAppComponent.Builder application(Application application);
+
+        TestAppComponent build();
+    }
+}
